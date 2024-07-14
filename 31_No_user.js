38ac855968aa2c145ed12daf47a3de1205cc94b2
Arbab Khan <148271563+Arbab24178@users.noreply.github.com>
2024-07-14 08:22:00 -0700

    Add files via upload

---
+++ 31_No_user.js
@@ -0,0 +1,17 @@
+"use strict";
+let userNames = ["Qais", "Mehdi", "Haya", "Zeeniya", "Admin"];
+userNames = [];
+if (userNames.length === 0) {
+    console.log("Your array in Empty We need to find users!");
+}
+else {
+    //Creating ForEach Loop on Array
+    userNames.forEach(oneUser => {
+        if (oneUser === "Admin") {
+            console.log(`Hello ${oneUser}, would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${oneUser},thanks for logging in again.`);
+        }
+    });
+}
