f0d8bfd29f6e890b5d23cc5e0dd4f5c311b69590
Arbab Khan <148271563+Arbab24178@users.noreply.github.com>
2024-07-16 06:20:42 -0700

    Add files via upload

---
+++ 34_Pizza.ts
@@ -0,0 +1,10 @@
+// creating Array
+let pizza = ["chicken tikka","Malai tikka","Fajtita"]
+
+// Using For-LOOP
+for(let onePizza of pizza){
+    console.log(`I love ${onePizza} pizza`);
+}
+
+// Print message outside of the for-loop
+console.log("Pizza is Love")
